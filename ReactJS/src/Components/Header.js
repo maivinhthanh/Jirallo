ad0f6eca5a41d26ce296b6385301b80d34423843
Mai Vĩnh Thành <34060324+maivinhthanh@users.noreply.github.com>
2019-09-08 21:52:36 +0700

    config package.json

--- ReactJS/src/Components/Header.js
+++ ReactJS/src/Components/Header.js
@@ -6,50 +6,50 @@ class Header extends Component {
       <div>
       <header>
       <nav className="nav-top">
-  {/* Created By Bogdan Nagorniy */}
-  <div className="logo">
-    <h1>
-      <a href="index.html">Trello</a>
-    </h1>
-  </div>
-  <ul className="nav_links list-unstyled">
-    <li className="nav-link-list">
-      <a href="index.html">
-        <span className="fa fa-home" />
-        <p>Home</p>
-      </a>
-    </li>
-    <li>
-    <Link to="/board">
-   <a href="/board">
-        <span className="fa fa-question" />
-        <p>Board</p>
-      </a>
-    </Link>
-    </li>
-    <li>
-      <a href="#services">
-        <span className="fa fa-cog" />
-        <p>Services</p>
-      </a>
-    </li>
-    <li>
-      <a href="#blog">
-        <span className="fa fa-clipboard" />
-        <p>Blog</p>
-      </a>
-    </li>
-    <li>
-    <Link to = "/login">
-    <a href="/login">
-        <span className="fa fa-users" />
-        <p>Team</p>
-      </a>
-    </Link>
-    </li>
-  </ul>
-</nav>
-</header>
+          {/* Created By Bogdan Nagorniy */}
+          <div className="logo">
+            <h1>
+              <a href="index.html"><img className="logo-menu" src="/logo-menu.jpg" /></a>
+            </h1>
+          </div>
+          <ul className="nav_links list-unstyled">
+            <li className="nav-link-list">
+              <a href="index.html">
+                <span className="fas fa-home" />
+                <p>Home</p>
+              </a>
+            </li>
+            <li>
+            <Link to="/board">
+          <a href="/board">
+                <span className="fas fa-question" />
+                <p>Board</p>
+              </a>
+            </Link>
+            </li>
+            <li>
+              <a href="#services">
+                <span className="fas fa-cog" />
+                <p>Services</p>
+              </a>
+            </li>
+            <li>
+              <a href="#blog">
+                <span className="fa fa-clipboard" />
+                <p>Blog</p>
+              </a>
+            </li>
+            <li>
+            <Link to = "/login">
+            <a href="/login">
+                <span className="fa fa-users" />
+                <p>Team</p>
+              </a>
+            </Link>
+            </li>
+          </ul>
+        </nav>
+        </header>
       </div>
     )
   }
